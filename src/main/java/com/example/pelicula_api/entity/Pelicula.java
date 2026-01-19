9e84f62ca753ef5f897d47052e4ee32cbf85eebe
Bryan13092005 <bryan.salvador@epn.edu.ec>
2026-01-18 22:00:08 -0500

    Default

--- src/main/java/com/example/pelicula_api/entity/Pelicula.java
+++ src/main/java/com/example/pelicula_api/entity/Pelicula.java
@@ -9,7 +9,7 @@ import jakarta.validation.constraints.*;
 public class Pelicula {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
